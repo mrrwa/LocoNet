2ffa4e3388a36b40b5ec944257524e28371c1872
plocher <john.plocher@gmail.com>
2014-11-10 21:04:57 -0800

    refactored ln_config.h, added LNPE reference to LICENSE

--- utility/ln_config.h
+++ utility/ln_config.h
@@ -31,27 +31,28 @@
  *
  * Thanks to the neatness and modularity of Alex, all the changes are isolated
  * to just the ln_config.h (this) file!
- ****************************************************************************/
+ *
+ ****************************************************************************
+ * 2014/11/10 - John Plocher
+ *
+ * refactored to put all the MEGA/UNO changes together in one conditional block
+ * to make it easier to port to other chipsets
+ *
+ ****************************************************************************
  */
 
-// For the UNO:
-// The RX port *MUST BE* the ICP pin (port PINB bit PB0, Arduino pin 8 on a '168) 
 
-// twk
-// For the Mega 2560 (should work with 1280, etc)
-// The RX port *MUST BE* the ICP5 pin (port PINL bit PL1, Arduino pin 48 on a 2560) 
 
-// twk 
-#ifdef PINL
-#define LN_RX_PORT  PINL
-#define LN_RX_DDR   DDRL
-#define LN_RX_BIT   PL1
-#else
-#define LN_RX_PORT  PINB
-#define LN_RX_DDR   DDRB
-#define LN_RX_BIT   PB0
+// jmp
+// figure out what board we are building for
+// TODO:  Add support for Leo and others...
+#ifdef PINL	//      For the Mega 2560 (should work with 1280, etc)
+#define _LNET_USE_MEGA
+#else		//	For the UNO:
+#define _LNET_USE_UNO
 #endif
 
+// Common defines
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 #endif
@@ -60,83 +61,85 @@
 #endif
 
 #undef  LN_SW_UART_TX_NON_INVERTED  // Normal is to be inverted...
-#define LN_BIT_PERIOD  (F_CPU / 16666)
+#define LN_BIT_PERIOD               (F_CPU / 16666)
+#define LN_TMR_PRESCALER              1
+#define LN_TIMER_TX_RELOAD_ADJUST   106 //  14,4 us delay borrowed from FREDI sysdef.h
+#define LN_TX_RETRIES_MAX            25
+
+
+// *****************************************************************************
+// *                                                              Arduino MEGA *
+// *                            The RX port *MUST BE* the ICP5 pin             *
+// *                            (port PINL bit PL1, Arduino pin 48 on a 2560)  *
+// *****************************************************************************
+#if defined( _LNET_USE_MEGA ) // twk
+
+#define LN_RX_PORT  PINL
+#define LN_RX_DDR   DDRL
+#define LN_RX_BIT   PL1
 
 // From sysdef.h:
-
-// twk
-#if defined(TIMER5_CAPT_vect)
 #define LN_SB_SIGNAL          TIMER5_CAPT_vect
-#else
-#define LN_SB_SIGNAL          TIMER1_CAPT_vect
-#endif
-
-// twk
-#if defined(TIMSK5)
 #define LN_SB_INT_ENABLE_REG  TIMSK5
 #define LN_SB_INT_ENABLE_BIT  ICIE5
 #define LN_SB_INT_STATUS_REG  TIFR5
 #define LN_SB_INT_STATUS_BIT  ICF5
-#elif defined(TIMSK)
-#define LN_SB_INT_ENABLE_REG  TIMSK
-#define LN_SB_INT_ENABLE_BIT  TICIE1
-#define LN_SB_INT_STATUS_REG  TIFR
-#define LN_SB_INT_STATUS_BIT  ICF1
-#else
-#define LN_SB_INT_ENABLE_REG  TIMSK1
-#define LN_SB_INT_ENABLE_BIT  ICIE1
-#define LN_SB_INT_STATUS_REG  TIFR1
-#define LN_SB_INT_STATUS_BIT  ICF1
-#endif
-
-// twk
-#if defined(TIMER5_COMPA_vect)
 #define LN_TMR_SIGNAL         TIMER5_COMPA_vect
-#else
-#define LN_TMR_SIGNAL         TIMER1_COMPA_vect
-#endif
-
-// twk
-#if defined(TIMSK5)
 #define LN_TMR_INT_ENABLE_REG TIMSK5
 #define LN_TMR_INT_STATUS_REG TIFR5
-#elif defined(TIMSK)
-#define LN_TMR_INT_ENABLE_REG TIMSK
-#define LN_TMR_INT_STATUS_REG TIFR
-#else
-#define LN_TMR_INT_ENABLE_REG TIMSK1
-#define LN_TMR_INT_STATUS_REG TIFR1
-#endif
-
-// twk
-#if defined(OCIE5A)
 #define LN_TMR_INT_ENABLE_BIT OCIE5A
 #define LN_TMR_INT_STATUS_BIT OCF5A
 #define LN_TMR_INP_CAPT_REG   ICR5      // [BA040319] added defines for:
 #define LN_TMR_OUTP_CAPT_REG  OCR5A     // ICR1, OCR1A, TCNT1, TCCR1B
 #define LN_TMR_COUNT_REG      TCNT5     // and replaced their occurence in
 #define LN_TMR_CONTROL_REG    TCCR5B    // the code.
+#define LN_INIT_COMPARATOR() { TCCR5A = 0; TCCR5B = 0x01; }    // no prescaler, normal mode
+
+// *****************************************************************************
+// *                                                               Arduino UNO *
+// *                              The RX port *MUST BE* the ICP pin            *
+// *                              (port PINB bit PB0, Arduino pin 8 on a '168) *
+// *****************************************************************************
+#elif defined( _LNET_USE_UNO )
+
+#define LN_RX_PORT  PINB
+#define LN_RX_DDR   DDRB
+#ifdef PB0	// bug/missing defines (some hardware core's ioXX.h files define one or the other)
+#define LN_RX_BIT   PB0
 #else
+#define LN_RX_BIT   PORTB0
+#endif
+
+// From sysdef.h:
+#define LN_SB_SIGNAL          TIMER1_CAPT_vect
+#define LN_SB_INT_ENABLE_REG  TIMSK1
+#define LN_SB_INT_ENABLE_BIT  ICIE1
+#define LN_SB_INT_STATUS_REG  TIFR1
+#define LN_SB_INT_STATUS_BIT  ICF1
+#define LN_TMR_SIGNAL         TIMER1_COMPA_vect
+#define LN_TMR_INT_ENABLE_REG TIMSK1
+#define LN_TMR_INT_STATUS_REG TIFR1
 #define LN_TMR_INT_ENABLE_BIT OCIE1A
 #define LN_TMR_INT_STATUS_BIT OCF1A
 #define LN_TMR_INP_CAPT_REG   ICR1      // [BA040319] added defines for:
 #define LN_TMR_OUTP_CAPT_REG  OCR1A     // ICR1, OCR1A, TCNT1, TCCR1B
 #define LN_TMR_COUNT_REG      TCNT1     // and replaced their occurence in
 #define LN_TMR_CONTROL_REG    TCCR1B    // the code.
-#endif
-
-    
-#define LN_TMR_PRESCALER      1
-  
-#define LN_TIMER_TX_RELOAD_ADJUST   106 //  14,4 us delay borrowed from FREDI sysdef.h
-
-// twk
-#if defined(TCCR5A)
-#define LN_INIT_COMPARATOR() { TCCR5A = 0; TCCR5B = 0x01; }    // no prescaler, normal mode
-#else
 #define LN_INIT_COMPARATOR() { TCCR1A = 0; TCCR1B = 0x01; }    // no prescaler, normal mode
-#endif
 
-#define LN_TX_RETRIES_MAX  25
+// *****************************************************************************
+// *                                                       Arduino --UNKNOWN-- *
+// *****************************************************************************
+#else
+#error "No Arduino Board/Processor selected"
 
-#endif
+#define LN_SB_INT_ENABLE_REG  TIMSK
+#define LN_SB_INT_ENABLE_BIT  TICIE1
+#define LN_SB_INT_STATUS_REG  TIFR
+#define LN_SB_INT_STATUS_BIT  ICF1
+#define LN_TMR_INT_ENABLE_REG TIMSK
+#define LN_TMR_INT_STATUS_REG TIFR
+
+#endif	// board type
+#endif	// include file
+
