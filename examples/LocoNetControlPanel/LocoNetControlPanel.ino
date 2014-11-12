69d01b341cd985d7d95cb7880dfc4381325380ad
plocher <john.plocher@gmail.com>
2014-11-11 16:14:05 -0800

    additional example programs

---
+++ examples/LocoNetControlPanel/LocoNetControlPanel.ino
@@ -0,0 +1,162 @@
+/*
+ *  Loconet Control Panel - simple demo
+ *  2014 John Plocher - Public Domain
+ *
+ *  Circuit:
+ *    Loconet on pins 8 (RX) and 7 (TX) (via a LocoShield or similar)
+ *    Push Buttons on pins 5 and 6 (making a connection to ground when pressed)
+ *    LED on pin 13 (lit when NORMAL)
+ *
+ *  Code:
+ *    Use the two pushbuttons to control a turnout somewhere else on the layout
+ *    by way of a Loconet layout control bus.
+ *        press the "N" button and genetate a loconet "set turnout to Normal" message,
+ *        press the "D" button and generate "set turnout to Diverging"
+ *        make sure the LED is on when the turnout is Normal, even if someone else controls
+ *          the turnout with a throttle or JMRI...
+ *
+ *    The turnout number is hardcoded in this sketch
+ */
+
+#include <LocoNet.h>
+
+// #define LN_TX_PIN     47	// MEGA
+// #define LN_TX_PIN     6		// UNO
+#define LN_TX_PIN     7		// LocoShield
+//      LN_RX_PIN     Hardcoded in library for UNO and MEGA.  Will not work with Leonardo (yet)
+
+#define LED_PIN      13
+#define N_BUTTON_PIN  6
+#define D_BUTTON_PIN  5
+#define LN_TURNOUT   25
+
+// Loconet turnout position definitions
+#define TURNOUT_NORMAL     1  // aka closed
+#define TURNOUT_DIVERGING  0  // thrown
+
+lnMsg  *LnPacket;          // pointer to a received LNet packet
+int     nbit1, nbit2;      // used to debounce "N" pushbutton
+int     dbit1, dbit2;      // used to debounce "D" pushbutton
+int     led_state;         // on (1) or off (0)
+int     nbutton, dbutton;  // on (1) or off (0)
+int     last_nbutton, last_dbutton;  // only want to generate one loconet message per button press
+
+void sendOPC_SW_REQ(int address, byte dir, byte on) {
+    lnMsg SendPacket ;
+    
+    int sw2 = 0x00;
+    if (dir) sw2 |= B00100000;
+    if (on)  sw2 |= B00010000;
+    sw2 |= (address >> 7) & 0x0F;
+    
+    SendPacket.data[ 0 ] = OPC_SW_REQ ;
+    SendPacket.data[ 1 ] = address & 0x7F ;
+    SendPacket.data[ 2 ] = sw2 ;
+    
+    LocoNet.send( &SendPacket );
+}
+
+// Some turnout decoders (DS54?) can use solenoids, this code emulates the digitrax 
+// throttles in toggling the "power" bit to cause a pulse
+void setLNTurnout(int address, byte dir) {
+    sendOPC_SW_REQ(address - 1, dir, 1);
+    sendOPC_SW_REQ(address - 1, dir, 0);
+}
+
+void setup() {
+    pinMode(LED_PIN,      OUTPUT); 
+    pinMode(N_BUTTON_PIN, INPUT_PULLUP); 
+    pinMode(D_BUTTON_PIN, INPUT_PULLUP); 
+    digitalWrite(LED_PIN, LOW);
+    LocoNet.init(LN_TX_PIN);
+    led_state = 0;
+    last_nbutton = last_dbutton = -1;
+}
+
+void loop() {  
+    
+    // read the various input bits
+    nbit1 = digitalRead(N_BUTTON_PIN);
+    dbit1 = digitalRead(D_BUTTON_PIN);
+    delay(10);  // wait for mechanical switch bounces to subside
+    nbit2 = digitalRead(N_BUTTON_PIN);
+    dbit2 = digitalRead(D_BUTTON_PIN);
+
+    // clean them up, interpret them and make decisions    
+    
+    // identical readings mean we have a good result
+    if (nbit1 == nbit2) {        // Was "N"ormal pushbutton pressed?
+        if (nbit1 == LOW) {
+            nbutton = 1;
+        } else {
+            nbutton = 0;
+        }
+    }
+    if (dbit1 == dbit2) {        // Was "D"iverging pushbutton pressed?
+        if (dbit1 == LOW) {
+            dbutton = 1;
+        } else {
+            dbutton = 0;
+        }
+    } else {  // no valid actions to perform
+        nbutton = 0;
+        dbutton = 0;
+    }
+    
+    
+    // now that we have clean inputs, implement the "business logic" that
+    // you want - in this case, the buttons should control a turnout...
+    if ((nbutton == 1) && (dbutton == 1)) {
+        // ERROR - someone is leaning on the fascia and pressing both buttons...
+        //         ignore it.
+    } else if ((nbutton == 1) && (nbutton == last_nbutton)) {
+        // Already processed this N button push, wait for the bozo to take their thumb off the button!
+    } else if ((dbutton == 1) && (dbutton == last_dbutton)) {
+        // Already processed this D button push, wait for the bozo to take their other thumb off the button!
+    } else if (nbutton == 1) {
+        // Set the turnout to Normal
+        led_state = 1;
+        setLNTurnout(LN_TURNOUT, TURNOUT_NORMAL);
+    } else if (dbutton == 1) {
+        // Set the turnout to Diverging
+        led_state = 0;
+        setLNTurnout(LN_TURNOUT, TURNOUT_DIVERGING);
+    } else {
+        // both must be 0 - nothing pressed, nothing to do
+    }
+    last_nbutton = nbutton;  // remember the state of the
+    last_dbutton = dbutton;  // buttons for next time...
+
+
+    // done with our "local" logic, now we need to check for any loconet packets that might
+    // influence our control panel, such as when someone else throws the turnout from a throttle.
+    // We want to catch these events so the LED follows the turnout's true state
+    
+    // Check for any received LocoNet packets
+    LnPacket = LocoNet.receive() ;
+    if( LnPacket ) {   
+        LocoNet.processSwitchSensorMessage(LnPacket);
+        // this function will call the specially named functions below...
+    }
+    
+    // Finally, update the LED to match the turnout position
+    digitalWrite(LED_PIN, led_state);
+}
+
+// Callbacks from LocoNet.processSwitchSensorMessage() ...
+// We tie into the ones connected to turnouts so we can capture anything
+// that can change (or indicatea change to) a turnout's position.
+
+void notifySensor( uint16_t Address, uint8_t State )
+{ /* ignore, not a turnout related action */ }
+
+void notifySwitchRequest( uint16_t Address, uint8_t Output, uint8_t Direction )
+{ if (Address == LN_TURNOUT) { led_state = Direction; } }
+
+void notifySwitchReport( uint16_t Address, uint8_t Output, uint8_t Direction )
+{ if (Address == LN_TURNOUT) { led_state = Direction; } }
+
+void notifySwitchState( uint16_t Address, uint8_t Output, uint8_t Direction )
+{ if (Address == LN_TURNOUT) { led_state = Direction; } }
+
+
---
+++ examples/LocoNetLayoutControl/LocoNetLayoutControl.ino
@@ -0,0 +1,246 @@
+/*
+ * LocoNet Layout Commander
+ *
+ * A standalone controller which will generate LocoNet commands when a button is pressed to:
+ *   Turn track power on and off, with a LED to indicate state.
+ *   Generate a master E-Stop, with a LED to indicate state.
+ *   Clear all slots in a command station
+ *
+ * RX and TX Loconet
+ *   Hardcoded to use ICP pin 8 (port PINB bit PB0) for LocoNet input and a user define'd pin for output/transmit
+ * 
+ * 3 pushbuttons and associated LEDs plus a Loconet Shield
+ *
+ * Copyright (c) 2012, 2014 John Plocher, released under the terms of the MIT License (MIT)
+ */
+
+#include <LocoNet.h>
+
+#define PowerOnPort      2    // Buttons connected to these pins
+#define PowerOffPort     3    // GPON GPOFF and ESTOP as well as Clear All Slots
+#define EStopPort        4
+#define SlotClearPort    5
+
+#define LNtxPort         6    // LocoNet Transmit pin (LocoShield uses pin7)
+
+#define PowerOnLEDPort  11    // Assume this pin has a LED+resistor attached...
+#define PowerOffLEDPort 12    // Assume this pin has a LED+resistor attached...
+#define EStopLEDPort    13    // Assume this pin has a LED+resistor attached...
+
+// Button press state
+// Current
+int GPonButton,    GPonButton1,   GPonButton2;
+int GPoffButton,   GPoffButton1,  GPoffButton2;
+int EStopButton,   EStopButton1,  EStopButton2;
+int ClearButton,   ClearButton1,  ClearButton2;
+
+// Last state processed - helps us ensure we don't repeat commands while a button is held down
+int lastGPon  = -1;
+int lastGPoff = -1;
+int lastEStop = -1;
+int lastClear = -1;
+
+int ClearIt   =  0;           // Should we clear slots when we get a slot status packet?
+    
+void setup(){
+    pinMode(PowerOnPort,     INPUT);   
+    pinMode(PowerOffPort,    INPUT);   
+    pinMode(SlotClearPort,   INPUT);   
+    pinMode(EStopPort,       INPUT); 
+    
+    pinMode(EStopLEDPort,    OUTPUT); 
+    pinMode(PowerOnLEDPort,  OUTPUT); 
+    pinMode(PowerOffLEDPort, OUTPUT); 
+    
+    digitalWrite(PowerOnLEDPort,  0);   // Power is in an unknown state
+    digitalWrite(PowerOffLEDPort, 0);   // 
+    digitalWrite(EStopLEDPort,    0);   // and not estopped
+ 
+    // Configure the serial port for 57600 baud
+    Serial.begin(57600);
+    Serial.println("LocoNet Controller");     
+    
+    // initialize the LocoNet interface
+    LocoNet.init(LNtxPort);
+}                   
+
+void setLNTurnout(int address, byte dir) {
+    sendOPC_SW_REQ( address - 1, dir, 1 );
+    sendOPC_SW_REQ( address - 1, dir, 0 );
+}
+
+void sendOPC_SW_REQ(int address, byte dir, byte on) {
+    lnMsg SendPacket ;
+    
+    int sw2 = 0x00;
+    if (dir) sw2 |= B00100000;
+    if (on)  sw2 |= B00010000;
+    sw2 |= (address >> 7) & 0x0F;
+    
+    SendPacket.data[ 0 ] = OPC_SW_REQ ;
+    SendPacket.data[ 1 ] = address & 0x7F ;
+    SendPacket.data[ 2 ] = sw2 ;
+    
+    LocoNet.send( &SendPacket );
+}
+
+void sendOPC_INPUT_REP(int address, byte on) {
+        lnMsg SendPacket;
+        
+        SendPacket.data[ 0 ] = OPC_INPUT_REP;  
+        SendPacket.data[ 1 ] = address & 0x7F;  // turnout address
+        int in2 = B01000000;
+        if (on)  in2 |= B00010000;
+        in2 |= (address >> 7) & 0x0F;
+        SendPacket.data[ 2 ] = in2;            // sw2 contains direction, on/off and hi nibble of address
+
+        LocoNet.send( &SendPacket ) ;
+}
+
+void sendOPC_GP(byte on) {
+        lnMsg SendPacket;
+        if (on) {
+            SendPacket.data[ 0 ] = OPC_GPON;  
+        } else {
+            SendPacket.data[ 0 ] = OPC_GPOFF;  
+        }
+        LocoNet.send( &SendPacket ) ;
+}
+
+void sendOPC_IDLE() {
+        lnMsg SendPacket;
+        SendPacket.data[ 0 ] = OPC_IDLE;  
+        LocoNet.send( &SendPacket ) ;
+}
+
+void send3bytePacket(int opcode, int slot, int spd) {
+        lnMsg SendPacket;
+
+        SendPacket.data[ 0 ] = opcode;
+        SendPacket.data[ 1 ] = slot;
+        SendPacket.data[ 2 ] = spd;  
+        LocoNet.send( &SendPacket );
+}
+void sendOPC_LOCO_SPD(int slot, int spd) {
+        send3bytePacket(OPC_LOCO_SPD,slot,spd);
+}
+
+void sendOPC_LOCO_DIRF(int slot, int dirf) {
+        send3bytePacket(OPC_LOCO_DIRF,slot,dirf);
+}
+
+void sendOPC_LOCO_SND(int slot, int snd) {
+        send3bytePacket(OPC_LOCO_SND,slot,snd);
+}
+
+void sendOPC_SLOT_STAT1(int slot, int stat) {
+        send3bytePacket(OPC_SLOT_STAT1,slot,stat);
+}
+
+void sendOPC_RQ_SL_DATA(int slot) {
+        send3bytePacket(OPC_RQ_SL_DATA,slot,0);
+}
+
+void processIncomingLoconetCommand(lnMsg* LnPacket) {
+    if( LnPacket )      {   
+        //LocoNet.processSwitchSensorMessage(LnPacket);
+        unsigned char opcode = (int)LnPacket->sz.command;
+        if (opcode == OPC_GPON)  {     
+            Serial.println("Power ON");     
+            digitalWrite(PowerOnLEDPort, 1);  
+            digitalWrite(PowerOffLEDPort, 0);
+            digitalWrite(EStopLEDPort, 0);
+        } else if (opcode == OPC_GPOFF) {
+            Serial.println("Power OFF");     
+            digitalWrite(PowerOnLEDPort, 0);
+            digitalWrite(PowerOffLEDPort, 1);
+        } else if (opcode == OPC_IDLE) {
+            digitalWrite(EStopLEDPort, 1);
+            Serial.println("EStop!"); 
+        } else if (opcode == OPC_SL_RD_DATA) {
+            if (ClearIt) {
+                int slot = LnPacket->sd.slot;
+                int stat = LnPacket->sd.stat;
+                Serial.print("Clear Slot:"); Serial.print(slot); Serial.print(":");  Serial.println(stat);
+                if (stat != 0) {
+                   sendOPC_LOCO_SPD(slot,  0);   // speed 0
+                   sendOPC_LOCO_DIRF(slot, 0);   // F0-4 off, Fwd
+                   sendOPC_LOCO_SND(slot,  0);   // F5-8 off
+                    // Don't need to turn off F9 and above because they should go away when track power is turned off...
+                   sendOPC_SLOT_STAT1(slot, 0);
+                }
+            }
+        } else {
+            // ignore the message...
+        }
+    }
+}
+
+void loop() {  
+    // Check for any received LocoNet packets
+    while (lnMsg *LnPacket = LocoNet.receive() ) {
+        processIncomingLoconetCommand( LnPacket );
+    }
+    // Debounce logic:
+    // ...Check for any buttons pushed, delay, read again...
+    GPonButton1  = digitalRead(PowerOnPort);
+    GPoffButton1 = digitalRead(PowerOffPort);
+    EStopButton1 = digitalRead(EStopPort);
+    ClearButton1 = digitalRead(SlotClearPort);
+    delay(5);
+    GPonButton2  = digitalRead(PowerOnPort);
+    GPoffButton2 = digitalRead(PowerOffPort);
+    EStopButton2 = digitalRead(EStopPort);
+    ClearButton2 = digitalRead(SlotClearPort);
+
+    // ...identical readings mean we have a good result
+    if (GPonButton1  == GPonButton2)  { GPonButton  = GPonButton1  ? 0 : 1; }
+    if (GPoffButton1 == GPoffButton2) { GPoffButton = GPoffButton1 ? 0 : 1; }
+    if (EStopButton1 == EStopButton2) { EStopButton = EStopButton1 ? 0 : 1; }
+    if (ClearButton1 == ClearButton2) { ClearButton = ClearButton1 ? 0 : 1; }
+
+                
+    if (lastGPon == -1) {
+        // need to initialize things the first time thru to ensure buttons don't all fire...
+        lastGPon = GPonButton;
+        lastGPoff = GPoffButton;
+        lastEStop = EStopButton;
+        lastClear = ClearButton;
+    } else {
+        // See if anything has changed since last time thru...
+
+        if (GPonButton != lastGPon) {                // GP_ON
+            lastGPon = GPonButton;
+            if (GPonButton) {
+                sendOPC_GP(1);
+            }
+            ClearIt = 0;
+                
+        }
+        if (GPoffButton != lastGPoff) {              // GP_OFF
+            lastGPoff = GPoffButton;
+            if (GPoffButton) {
+                sendOPC_GP(0);
+            }
+            ClearIt = 0;
+        }
+        if (EStopButton != lastEStop) {              // E_STOP
+            lastEStop = EStopButton;
+            if (EStopButton) {
+                sendOPC_IDLE();
+            }
+            ClearIt = 0;
+        }
+        if (ClearButton != lastClear) {              // Clear all Slots
+            lastClear = ClearButton;
+            if (ClearButton) {
+                ClearIt = 1;
+                // query all the slots, let the handler clear things
+                for (int slot = 0; slot < 120; slot++) {
+                    sendOPC_RQ_SL_DATA(slot);
+                }
+                ClearIt = 0;
+            }
+        }     
+    }
+}
